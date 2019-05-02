534e6d4b3152da28c5003d92c323fd3c082bcb7d
Roman Safin <gordonfreeman2332@gmail.com>
2019-05-03 02:40:02 +0300

    data complete

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ osmosis
    .get('http://kraskisnab.ru.com/')
    .find('li.moto-widget-menu-item')
    .follow('li.moto-widget-menu-item a[href]')
-   .paginate('a.moto-widget-store-paginator-link')
+   .find('div.moto-widget-store-paginator')
+   .follow('a.moto-widget-store-paginator-link[href]')
    .find('.moto-widget-store-main_item')
+   .follow('a.moto-widget-store-main_item-title[href]')
+   .find('section.moto-section > div.moto-widget-row[2]')
    .set({
-       'title': 'a > span',
+       'title': 'div.moto-widget-text-content > h2',
+       'brend': 'ul.moto-widget-store-detail-product_properties > li[2]',
+       'code': 'ul.moto-widget-store-detail-product_properties[2] > li[2]',
        'imgsrc': '.moto-widget-image-link img @data-src',
-       'price': 'div.store-product-element-container > div'
+       'price': 'div.moto-widget-store-main_item-price > h4',
+       'text': ['div.moto-widget-store-detail-text > p']
     })
    .log(console.log)   // включить логи
    .data((data)=>{
-    //    console.log(data)
+        console.log(data)
         savedData.push(data)
     })
 
